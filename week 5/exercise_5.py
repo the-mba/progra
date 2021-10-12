2eef61399e309080b45207e9cccdff2a8f038325
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-12 23:24:28 +0200

    --- Auto-Git Commit ---

--- week 5/exercise_5.py
+++ week 5/exercise_5.py
@@ -11,8 +11,36 @@ for i in range(PIN_LENGTH):
 balance = random.randrange(MIN_MONEY, MAX_MONEY)
 
 remaining_attempts = 3
-pin_attempt = ""
 while remaining_attempts > 0:
-    
+    pin_attempt = input("Sir, we require your PIN number: ")
+    if pin_attempt == pin:
+        break
+    remaining_attempts -= 1
 
-pin_attempt = input("Sir, we require your PIN number: ")
+if remaining_attempts == 0:
+    exit()
+
+menu = """Welcome
+---------------------
+1- Deposit
+2- Cash withdrawal
+3- Exit
+Choose the operation: """
+
+while True:
+    action = input(menu)
+    if action == "1" or action == "Deposit":
+        amount = input("Please enter the amount to deposit (in EUROS): ")
+        balance += amount
+        print("You withdrew " + str(amount) + "€ succesfully. Your current balance is: " + str(balance) + "€")
+    elif action == "2" or action == "Cash withdrawal":
+        amount = input("Please enter the amount to withdrawal (in EUROS): ")
+        if amount <= balance:
+            balance -= amount
+            print("You deposited " + str(amount) + "€ succesfully. Your current balance is: " + str(balance) + "€")
+        else:
+            print("Insufficient funds. Your current balance is: " + str(balance))
+    elif action == "3" or action == "Exit":
+        exit()
+
+
