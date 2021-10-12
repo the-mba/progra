bbd03f6fad6cc39b31a1f7c93c2603039bd7bde8
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-12 19:27:18 +0200

    --- Auto-Git Commit ---

--- week 5/exercise_1.py
+++ week 5/exercise_1.py
@@ -5,12 +5,12 @@ tries = 0
 found = False
 while not found:
     print(number)
-    attempt = int(input("Guess the number:"))
+    attempt = int(input("Guess the number: "))
     if attempt == number:
         found = True
     tries += 1
     if tries == 1:
-        print("You have tried " + str(tries) + " time")
+        print("You have tried " + str(tries) + " time.")
     else:
-        print("You have tried " + str(tries) + " times")
-print("Congratulations, you found the number")
+        print("You have tried " + str(tries) + " times.")
+print("Congratulations, you found the number.")
