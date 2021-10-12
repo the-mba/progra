e29493a9ea99c9f496c3c1b8213feff1991deaa8
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-12 19:30:33 +0200

    --- Auto-Git Commit ---

---
+++ week 5/exercise_2.py
@@ -0,0 +1,17 @@
+import random
+
+valid = False
+while not valid:
+    print("I NEED TWO INTEGERS, A AND B, SUCH THAT B IS 5 OR MORE BIGGER THAN A.")
+    a = int(input("Enter A: ")
+    b = int(input("Enter B: ")
+    if a + 5 < b:
+        valid = True
+
+numbers = [int(random.randrange(a, b))]
+while len(numbers) < 5:
+    number = int(random.randrange(a, b))
+    if ( number + numbers[-1] ) % 2 == 1:
+        numbers.append(number)
+
+print(numbers)
