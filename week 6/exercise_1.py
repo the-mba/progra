d7493597b57dc4cb8263e1b4784002c5917038d0
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-15 19:52:49 +0200

    --- Auto-Git Commit ---

--- week 6/exercise_1.py
+++ week 6/exercise_1.py
@@ -1,6 +1,6 @@
 list_1 = [1, 2, 3, 4, 5]
-list[2] = list[4]
-list[4] = 55
-print(list[2])
-list[2] = 33
-print(list[2], list[4])
+list_1[2] = list_1[4]
+list_1[4] = 55
+print(list_1[2])
+list_1[2] = 33
+print(list_1[2], list_1[4])
