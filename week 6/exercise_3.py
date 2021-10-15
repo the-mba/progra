df1e99ec80ca7193a12ed20df3b03d7d32bb71d8
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-15 20:11:17 +0200

    --- Auto-Git Commit ---

--- week 6/exercise_3.py
+++ week 6/exercise_3.py
@@ -1,9 +1,17 @@
+import random
 
-
+list_float = []
 valid = False
+total = 0
 
 while not valid:
     length = input("Please, input the desired length for the float list: ")
     if int(length) > 0:
         valid = True
 
+for i in range(length):
+    list_float.append(random.randrange(1, 50))
+    total += int(list_float[-1])
+
+print(list_float)
+print(total)
