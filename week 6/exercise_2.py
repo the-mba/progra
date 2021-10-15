4a6ea4a09618e0c32a16edfbd2cf8abf970abd05
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-15 19:58:15 +0200

    --- Auto-Git Commit ---

--- week 6/exercise_2.py
+++ week 6/exercise_2.py
@@ -1,9 +1,19 @@
 list_1 = [] * 30
-list_2 = [] * 30
+list_3 = [] * 30
+
 
 for i in range(30):
     list_1[i] = i * i
 
+list_2 = list_1
+
+print("List 1 is: " + str(list_1))
+print("List 2 is: " + str(list_2))
+
+
 for i in range(30):
-    list_2[i] = 30 - i
+    list_3[i] = 30 - i
+
+for i in range(30):
+    list_4[i] = list_3[i]
 
