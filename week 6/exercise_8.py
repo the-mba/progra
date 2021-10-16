621a9b032babd149723b16b45d984b4b0939138e
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-16 18:21:56 +0200

    --- Auto-Git Commit ---

--- week 6/exercise_8.py
+++ week 6/exercise_8.py
@@ -2,6 +2,8 @@ sentence = input("Please enter the sentence:\n")
 
 tup = ()
 for c in sentence:
+    c = c.upper()
     if c not in tup:
-        tup += (c.upper(),)
+        tup += (c,)
 
+print(tup)
