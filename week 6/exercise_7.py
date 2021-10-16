c0ffd0afd566f3f0ed29b941fafbde7620af0c3e
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-16 18:12:09 +0200

    --- Auto-Git Commit ---

--- week 6/exercise_7.py
+++ week 6/exercise_7.py
@@ -5,7 +5,9 @@ total = int(input("Please enter the total amount of money: "))
 minimum = 0
 
 for i in types_of_notes_and_coins:
+    print(minimum)
+    print(total)
     minimum += total // i
     total -= total // i
 
-print("The minimum number of notes and coins is: " + str(total))
+print("The minimum number of notes and coins is: " + str(minimum))
