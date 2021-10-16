52d2dd48af1d8996db88ba5ae94b8478336cce59
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-16 17:59:08 +0200

    --- Auto-Git Commit ---

--- week 6/exercise_5.py
+++ week 6/exercise_5.py
@@ -1,5 +1,8 @@
 import random
 
+MIN = 1
+MAX = 10
+
 memo = []
 
 print("\nHello! Today I will be guessing your favorite number!\n")
@@ -7,7 +10,9 @@ print("\nHello! Today I will be guessing your favorite number!\n")
 found = False
 
 while not found:
-    number = random.randint(1, 100)
+    number = random.randint(MIN, MAX)
+    while number in memo:
+        number = random.randint(MIN, MAX)
     memo.append(number)
     answer = input("Is " + str(number) + " your number (Yes/No)? ")
     if answer == "Yes":
