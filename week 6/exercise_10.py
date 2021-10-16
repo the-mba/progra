0bb02a6ceb631aa006e99297920fe04036e249c4
Mario Balbuena Azcona <100472070@alumnos.uc3m.es>
2021-10-16 18:38:13 +0200

    --- Auto-Git Commit ---

---
+++ week 6/exercise_10.py
@@ -0,0 +1,45 @@
+import time, random
+
+NUMBER_OF_VALUES = 100000
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+
+start = time.time()
+
+list_1 = []
+
+for i in range(NUMBER_OF_VALUES):
+    list_1.append(random.randint(MIN_VALUE, MAX_VALUE))
+
+end = time.time()
+
+time_1 = end - start
+
+
+start = time.time()
+
+list_2= []
+
+for i in range(NUMBER_OF_VALUES):
+    list_1 += random.randint(MIN_VALUE, MAX_VALUE)
+
+end = time.time()
+
+time_2 = end - start
+
+
+start = time.time()
+
+list_3 = []
+
+for i in range(NUMBER_OF_VALUES):
+    list_1 = list_1 + [random.randint(MIN_VALUE, MAX_VALUE)]
+
+end = time.time()
+
+time_3= end - start
+
+print("Time with append was: " + str(time_1))
+print("Time with += was: " + str(time_2))
+print("Time with + was: " + str(time_3))
